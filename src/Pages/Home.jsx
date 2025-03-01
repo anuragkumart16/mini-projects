c695c74762099e3c2d8968e41c16bea3e7561452
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-03-01 10:41:28 +0530

    added little changes to the homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom"
 function Home(){
     return (
         <div>
-            <h1>Home</h1>
+            <Link to='/qr'>Qr Generator</Link>
         </div>
     )
 }

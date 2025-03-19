5f11c590a06d8ce7c5afb95b6256f2ecf5023424
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-03-19 16:07:13 +0530

    added changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,35 @@
-
+import { useNavigate } from "react-router-dom";
 import './css/home.css'
 import Navbar from "../components/Navbar/Navbar"
 
 
 function Home(){
+    const navigate = useNavigate()
     const array = [
         {
             source: 'card-img/qr.png',
             title : 'QR Generator',
-            desc : 'Creates QR code using given text'
+            desc : 'Creates QR code using given text',
+            page :'/qr'
         },
         {
             source: 'card-img/notes.png',
             title : 'Note Taking App',
-            desc : 'Lets you save your thoughts in organised way'
+            desc : 'Lets you save your thoughts in organised way',
+            page :'/notes'
+
         },
         {
             source: 'card-img/book.png',
             title : 'Random Book Suggestions',
-            desc : 'Give Random Book Reading Suggestions'
+            desc : 'Give Random Book Reading Suggestions',
+            page :'/book'
         },
         {
             source: 'card-img/signin.png',
             title : 'Typical Authentication Page',
-            desc : 'Are you a member? Login else signup!'
+            desc : 'Are you a member? Login else signup!',
+            page :'/auth'
         }
     ]
     return (
@@ -33,7 +39,9 @@ function Home(){
             {
                 array.map((element,index)=>{
                     return(
-                        <div className="home-card" key={index}>
+                        <div className="home-card" key={index} onClick={()=>{
+                            navigate(element['page'])
+                        }}>
                 <img src={element.source} alt="" className="home-card-image" />
                 <h3 className="home-card-heading
                 ">{element.title}</h3>
